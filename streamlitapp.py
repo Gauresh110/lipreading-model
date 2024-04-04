b282e5a67f9edeae8aec8a18890f8b817db04729
Gauresh110 <122960510+Gauresh110@users.noreply.github.com>
2024-04-04 23:57:03 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -2,84 +2,10 @@
 import streamlit as st
 import os 
 import imageio 
-import tensorflow as tf
-from typing import List
-import cv2
-from tensorflow.keras.models import Sequential 
-from tensorflow.keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
 
-vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
-char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
-# Mapping integers back to original characters
-num_to_char = tf.keras.layers.StringLookup(
-    vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
-)
-
-def load_video(path:str) -> List[float]: 
-    #print(path)
-    cap = cv2.VideoCapture(path)
-    frames = []
-    for _ in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))): 
-        ret, frame = cap.read()
-        frame = tf.image.rgb_to_grayscale(frame)
-        frames.append(frame[190:236,80:220,:])
-    cap.release()
-    
-    mean = tf.math.reduce_mean(frames)
-    std = tf.math.reduce_std(tf.cast(frames, tf.float32))
-    return tf.cast((frames - mean), tf.float32) / std
-    
-def load_alignments(path:str) -> List[str]: 
-    #print(path)
-    with open(path, 'r') as f: 
-        lines = f.readlines() 
-    tokens = []
-    for line in lines:
-        line = line.split()
-        if line[2] != 'sil': 
-            tokens = [*tokens,' ',line[2]]
-    return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
-
-def load_data(path: str): 
-    path = bytes.decode(path.numpy())
-    file_name = path.split('/')[-1].split('.')[0]
-    # File name splitting for windows
-    file_name = path.split('\\')[-1].split('.')[0]
-    video_path = os.path.join('..','data','s1',f'{file_name}.mpg')
-    alignment_path = os.path.join('..','data','alignments','s1',f'{file_name}.align')
-    frames = load_video(video_path) 
-    alignments = load_alignments(alignment_path)
-    
-    return frames, alignments
-
-def load_model() -> Sequential: 
-    model = Sequential()
-
-    model.add(Conv3D(128, 3, input_shape=(75,46,140,1), padding='same'))
-    model.add(Activation('relu'))
-    model.add(MaxPool3D((1,2,2)))
-
-    model.add(Conv3D(256, 3, padding='same'))
-    model.add(Activation('relu'))
-    model.add(MaxPool3D((1,2,2)))
-
-    model.add(Conv3D(75, 3, padding='same'))
-    model.add(Activation('relu'))
-    model.add(MaxPool3D((1,2,2)))
-
-    model.add(TimeDistributed(Flatten()))
-
-    model.add(Bidirectional(LSTM(128, kernel_initializer='Orthogonal', return_sequences=True)))
-    model.add(Dropout(.5))
-
-    model.add(Bidirectional(LSTM(128, kernel_initializer='Orthogonal', return_sequences=True)))
-    model.add(Dropout(.5))
-
-    model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
-
-    model.load_weights(os.path.join('..','models','checkpoint'))
-
-    return model
+import tensorflow as tf 
+from utils import load_data, num_to_char
+from modelutil import load_model
 
 # Set the layout to the streamlit app as wide 
 st.set_page_config(layout='wide')
